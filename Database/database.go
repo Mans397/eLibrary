adf1954abe74609a25d7a135faf4b64ca13dc9e5
mansurserikov <Mansurserikov889@gmail.com>
2024-12-13 12:04:06 +0500

    rewrite DatabaseInit

--- Database/database.go
+++ Database/database.go
@@ -1,1 +1,45 @@
 package Database
+
+import (
+	"fmt"
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5433
+	user     = "postgres"
+	password = "rootroot"
+	dbname   = "eLibrary"
+)
+
+var DB *gorm.DB
+
+func Init() {
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname)
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+	//	"password=%s dbname=%s sslmode=disable",
+	//	host, port, user, password, dbname)
+	//db, err := sql.Open("postgres", psqlInfo)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer db.Close()
+	//
+	//err = db.Ping()
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	fmt.Println("Successfully connected!")
+}
--- serverConnection/connectionHandler.go
+++ serverConnection/connectionHandler.go
@@ -7,7 +7,7 @@ import (
 
 const port = ":8080"
 
-func Connect() {
+func ConnectToServer() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.HandleFunc("/", MainHandler)
 	http.HandleFunc("/data/json", DataJsonHandler)
