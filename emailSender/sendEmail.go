0285dc805741ed476d736f683cf3d5c9b68727bb
Prrost <prrostik@gmail.com>
2024-12-28 12:00:43 +0500

    add gorutines on email sending

--- emailSender/sendEmail.go
+++ emailSender/sendEmail.go
@@ -4,28 +4,32 @@ import (
 	db "github.com/Mans397/eLibrary/Database"
 	"gopkg.in/gomail.v2"
 	"log"
+	"sync"
 )
 
 func SendEmailAll(text *string) error {
 	var users []db.User
+	wg := new(sync.WaitGroup)
 
 	result := db.DB.Find(&users)
 	if result.Error != nil {
 		log.Printf("failed to fetch users: %v", result.Error)
 		return result.Error
 	}
-
+	log.Printf("Starting sending email for %d users", len(users))
 	for _, user := range users {
-		err := SendEmail(user.Email, text)
-		if err != nil {
-			log.Printf("failed to send email: %v", err)
-		}
+		wg.Add(1)
+		go SendEmail(user.Email, text, wg)
+
 	}
+	wg.Wait()
+	log.Printf("Finished sending emails")
 
 	return nil
 }
 
-func SendEmail(email string, text *string) error {
+func SendEmail(email string, text *string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	m := gomail.NewMessage()
 	m.SetHeader("From", "elibrarysender@gmail.com")
 	m.SetHeader("To", email)
@@ -36,8 +40,8 @@ func SendEmail(email string, text *string) error {
 
 	if err := d.DialAndSend(m); err != nil {
 		log.Printf("Failed to send email: %v, User: %v\n", err, email)
-		return err
+
 	}
 	log.Println("Email sent successfully to user:", email)
-	return nil
+
 }
